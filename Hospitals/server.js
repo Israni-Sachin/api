0436959bd80ee58bc57a99b82f150ec192e8d643
Israni-Sachin <sachinisrani586@gmail.com>
2023-10-18 13:39:42 +0530

    env update

--- Hospitals/server.js
+++ Hospitals/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3500;
 app.use(express.json());
 
-const routes = require('./Routes');
-const loginAuth = require('./Controllers/logging.controllers')
-const verifyToken = require('./middlewares/auth/jwt')
+// const routes = require('./Routes');
+// const loginAuth = require('./Controllers/logging.controllers')
+// const verifyToken = require('./middlewares/auth/jwt')
 
-app.post('/login', loginAuth)
-app.use('/hospital',  routes);
+// app.post('/login', loginAuth)
+// app.use('/hospital',  routes);
+
+
 
 app.use('/', (req, res) => {
     res.send({ Status: 404, Message: "Hello khana khake jana ho" });
