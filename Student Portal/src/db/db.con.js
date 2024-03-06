5665d85e5a1e0391ccd37e2069a8a1830d2b82f5
Sachin <sachinisrani586@gmail.com>
2024-03-06 20:03:29 +0530

    Update db.con.js

--- Student Portal/src/db/db.con.js
+++ Student Portal/src/db/db.con.js
@@ -2,7 +2,8 @@ const { DB_URL } = require("../configs/db.config");
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/Student_Portal")
+// mongoose.connect("mongodb://127.0.0.1:27017/Student_Portal")
+mongoose.connect("mongodb+srv://sachinisrani56:sachin%401234@studentportal.t0bysyw.mongodb.net/")
 // mongoose.connect("mongodb://127.0.0.1/:27017?dbName=Student_Portal")
     .then(() => console.log("MongoDb connected"))
     .catch((err) => console.log(err));
