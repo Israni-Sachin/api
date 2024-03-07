dae4f8594e24f496a1474aa919116df392682593
Sachin <sachinisrani586@gmail.com>
2024-03-07 10:10:50 +0530

    update

--- Student Portal/src/server.js
+++ Student Portal/src/server.js
@@ -9,7 +9,8 @@ const { apiDocs } = require('./swagger');
 const app = express();
 // app.use('/api-docs', swaggerui.serve, swaggerui.setup(apiDocs));
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use(cors({ origin: '*' }));
