fd358f0b89ec8b39d2731d5d14651b9be12f742a
Sachin <sachinisrani586@gmail.com>
2024-03-15 12:31:53 +0530

    Update db.con.js

--- Student Portal/src/db/db.con.js
+++ Student Portal/src/db/db.con.js
@@ -10,9 +10,9 @@ let obj = {
 }
 
 let { MONGO_INITDB_ROOT_PASSWORD, MONGO_INITDB_ROOT_USERNAME, RAILWAY_TCP_PROXY_DOMAIN, RAILWAY_TCP_PROXY_PORT } = obj
-mongoose.connect("mongodb://127.0.0.1:27017/Student_Portal")
+// mongoose.connect("mongodb://127.0.0.1:27017/Student_Portal")
 // mongoose.connect(`mongodb://${{ MONGO_INITDB_ROOT_USERNAME }}:${{ MONGO_INITDB_ROOT_PASSWORD }}@${{ RAILWAY_TCP_PROXY_DOMAIN }}:${{ RAILWAY_TCP_PROXY_PORT }}`)
-// mongoose.connect("mongodb://mongo:G53fdd3bd6CeC64DCd5A56eBH4H6adHa@roundhouse.proxy.rlwy.net:12774")
+mongoose.connect("mongodb://mongo:G53fdd3bd6CeC64DCd5A56eBH4H6adHa@roundhouse.proxy.rlwy.net:12774")
     // mongoose.connect("mongodb+srv://sachinisrani56:sachin%401234@studentportal.t0bysyw.mongodb.net/")
     // mongoose.connect("mongodb://127.0.0.1/:27017?dbName=Student_Portal")
     .then(() => console.log("MongoDb connected"))
