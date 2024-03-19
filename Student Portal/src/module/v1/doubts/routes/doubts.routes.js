a975c126eb9a0e98acd0046d8b7e9209b0ea7c55
Sachin <sachinisrani586@gmail.com>
2024-03-19 18:58:26 +0530

    Update doubts.routes.js

--- Student Portal/src/module/v1/doubts/routes/doubts.routes.js
+++ Student Portal/src/module/v1/doubts/routes/doubts.routes.js
@@ -12,10 +12,10 @@ const doubtsRoutes = (app) => {
     // app.post('/doubts-list', verifyToken, roleValidator(['teacher']), controllers.doubtsList)
 
     // app.post('/doubts', verifyToken, roleValidator(['teacher']), validator(doubtsAddSchema), controllers.doubtsAdd);
-    app.post('/doubts', verifyToken, roleValidator(['teacher']), validator(doubtsAddSchema), controllers.doubtsAdd);
+    app.post('/doubts', verifyToken, roleValidator(['user']), validator(doubtsAddSchema), controllers.doubtsAdd);
     app.post('/doubtst', verifyToken, roleValidator(['teacher']), validator(doubtsAddSchema), controllers.doubtsAddt);
 
-    app.put('/doubts', verifyToken, roleValidator(['teacher']), validator(doubtsAddSchema), controllers.doubtsUpdate);
+    app.put('/doubts', verifyToken, roleValidator(['user']), validator(doubtsAddSchema), controllers.doubtsUpdate);
     app.put('/doubtst', verifyToken, roleValidator(['teacher']), validator(doubtsAddSchema), controllers.doubtsUpdatet);
 
     // app.delete('/doubts', verifyToken, roleValidator(['user']), validator(doubtsDelSchema), controllers.doubtsDelete);
