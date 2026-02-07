860aae99f358526c48c36cc2d692c8b4a1e588ab
SachinIsrani <sachin.israni@asint.net>
2026-02-07 15:27:12 +0530

    Image and video required false

--- Karva Backend/src/common/common-schemas.js
+++ Karva Backend/src/common/common-schemas.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const imageSchema = new mongoose.Schema({
     public_id: {
         type: String,
-        required: true
+        required: false
     },
     image_url: {
         type: String,
-        required: true
+        required: false
     }
 }, { _id: false });
 
